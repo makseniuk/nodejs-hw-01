34f049562815e61dcf7a7109fc4e57029269b507
makseniuk <144346873+makseniuk@users.noreply.github.com>
2024-07-08 16:24:24 +0200

    0.0.4

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+    try {
+        const emptyContacts = [];
+
+        await fs.writeFile(PATH_DB, JSON.stringify(emptyContacts, null, 2));
+
+        console.log('Successfully removed all contacts.');
+    } catch (error) {
+        console.log('Error removing contacts:', error);
+    }
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
+
+        if (contacts.lenght > 0) {
+            contacts.pop();
+
+            await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+            console.log('Successfuly remove last contacts.');
+        } else {
+            console.log('No contacts to remove.');
+        }
+    } catch (error) {
+        console.log('Error removing last contacts:', error);
+    }
+};
 
 removeLastContact();
